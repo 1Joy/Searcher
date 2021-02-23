09448fa15b27da7e9a64099c52e89e0e0167e520
jiangxin@singularityclub.net <jiangxin@singularityclub.net>
2021-02-23 22:12:29 +0800

    完善主界面和设置界面

--- Searcher/Searcher/ViewModels/MainWindowViewModel.cs
+++ Searcher/Searcher/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Searcher.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel:NotificationObject
     {
+        private Visibility mainPageVisibility;
+
+        public Visibility MainPageVisibility
+        {
+            get { return mainPageVisibility; }
+            set {
+                mainPageVisibility = value;
+                RaisePrepertyChange("MainPageVisibility");
+            }
+        }
+
+        private Visibility filePageVisibility;
+
+        public Visibility FilePageVisibility
+        {
+            get { return filePageVisibility; }
+            set {
+                filePageVisibility = value;
+                RaisePrepertyChange("FilePageVisibility");
+            }
+        }
+
+
     }
 }
