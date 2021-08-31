894f07f2bea1be19391499709af35e91efddc717
jiangxin@singularityclub.net <jiangxin@singularityclub.net>
2021-08-31 22:50:06 +0800

    界面样式

--- Searcher/Searcher/ViewModels/MainWindowViewModel.cs
+++ Searcher/Searcher/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,73 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows;
 
 namespace Searcher.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
+        public DelegateCommand<string> WindowControlCommand { get; private set; }
+
+        /// <summary>
+        /// 当前窗体显示状态
+        /// </summary>
+        private WindowState _currentWindowState = WindowState.Normal;
+        public WindowState CurrentWindowState
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => _currentWindowState;
+            set { _currentWindowState = value;
+                RaisePropertyChanged();
+            }
         }
+
+        /// <summary>
+        /// 最大化图标
+        /// </summary>
+        private string _maxBtnContent = "\ue62b";
+        public string MaxBtnContent
+        {
+            get => _maxBtnContent;
+            set { _maxBtnContent = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+
 
         public MainWindowViewModel()
         {
+            InitCommand();
+        }
 
+        /// <summary>
+        /// 初始化命令
+        /// </summary>
+        private void InitCommand()
+        {
+            WindowControlCommand = new DelegateCommand<string>(WindowControl);
+        }
+
+        /// <summary>
+        /// 控制窗体显示，最小化，最大化
+        /// </summary>
+        /// <param name="controlStr"></param>
+        private void WindowControl(string controlStr)
+        {
+            if (controlStr == "min")
+            {
+                CurrentWindowState = WindowState.Minimized;
+            }
+            else if (controlStr == "max"&&CurrentWindowState==WindowState.Normal)
+            {
+                CurrentWindowState = WindowState.Maximized;
+                MaxBtnContent = "\ue65b";
+            }
+            else if (controlStr == "max" && CurrentWindowState == WindowState.Maximized)
+            {
+                CurrentWindowState = WindowState.Normal;
+                MaxBtnContent = "\ue62b";
+            }
         }
     }
 }
